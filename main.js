f44255b0ba30b9c26e058021be86920ea577099c
groovygamer <99459849+groovygamer@users.noreply.github.com>
2023-08-09 11:21:39 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log(results);
+}
+function gotPoses(results)
+{
+    if(results.lenght>0);
+    {
+        console.log(results);
+    }
+}
